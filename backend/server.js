22a1c11ac8af3c175d67ebda6229b99a83376391
Isramss <isra.msadak@gmail.com>
2025-12-29 15:11:30 +0100

    Backend Express : Server operationnel

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({ message: "API WedPresta opérationnelle 🥳" });
+});
+
+// Port
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Le serveur lancé sur le port ${PORT}`);
+});

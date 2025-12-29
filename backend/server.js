962c5b07eb4759ae4f9183e340e680cff71e180e
Isramss <isra.msadak@gmail.com>
2025-12-29 15:39:30 +0100

    update PORT

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 });
 
 // Port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Le serveur lancé sur le port ${PORT}`);

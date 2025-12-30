6c0055c6bfaa9a8d78fbe510aef91ae2d78189b9
Isramss <isra.msadak@gmail.com>
2025-12-30 11:22:19 +0100

    feat(auth): middleware JWT (authenticateToken) + route /api/auth/profile

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,7 +1,9 @@
 // Ma première route securisé
 const express = require("express");
-const { register, login } = require("../controllers/auth.controller");
 const router = express.Router();
+
+const { register, login } = require("../controllers/auth.controller");
+const { authenticateToken } = require("../middleware/auth.middleware");
 
 router.get("/test", (req, res) => {
   res.json({ message: "Route auth OK" });
@@ -10,5 +12,12 @@ router.get("/test", (req, res) => {
 router.post("/register", register);
 
 router.post("/login", login);
+
+router.get("/profile", authenticateToken, (req, res) => {
+  res.json({
+    message: "Utilisateur authentifié",
+    user: req.user,
+  });
+});
 
 module.exports = router;
